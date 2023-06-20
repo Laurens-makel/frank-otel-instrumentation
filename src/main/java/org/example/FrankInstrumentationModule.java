f6e29af240240fa365ea00a4638a2a03706b815e
Mäkel, L. (Laurens) <laurens.makel@nn-group.com>
2023-06-20 21:35:37 +0200

    Missing annotation and public constructor

--- src/main/java/org/example/FrankInstrumentationModule.java
+++ src/main/java/org/example/FrankInstrumentationModule.java
@@ -1,15 +1,16 @@
 package org.example;
 
+import com.google.auto.service.AutoService;
 import io.opentelemetry.javaagent.extension.instrumentation.InstrumentationModule;
 import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
 
 import java.util.List;
 
 import static java.util.Collections.singletonList;
-
+@AutoService(InstrumentationModule.class)
 public class FrankInstrumentationModule extends InstrumentationModule {
 
-    protected FrankInstrumentationModule() {
+    public FrankInstrumentationModule() {
         super("frank-framework", "frank-framework-7.7");
     }
 
