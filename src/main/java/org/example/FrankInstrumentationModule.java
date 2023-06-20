7fc7c121818c8c851221a6bdabefbd4b98b74ebf
Mäkel, L. (Laurens) <laurens.makel@nn-group.com>
2023-06-20 22:40:11 +0200

    Declare additional helper classnames

--- src/main/java/org/example/FrankInstrumentationModule.java
+++ src/main/java/org/example/FrankInstrumentationModule.java
@@ -3,7 +3,12 @@ package org.example;
 import com.google.auto.service.AutoService;
 import io.opentelemetry.javaagent.extension.instrumentation.InstrumentationModule;
 import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
+import org.example.extractors.FrankPipeAttributeExtractor;
+import org.example.extractors.FrankPipeSpanNameExtractor;
+import org.example.extractors.FrankPipeStatusExtractor;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static java.util.Collections.singletonList;
@@ -17,5 +22,16 @@ public class FrankInstrumentationModule extends InstrumentationModule {
     @Override
     public List<TypeInstrumentation> typeInstrumentations() {
         return singletonList(new FrankInstrumentation());
+    }
+
+    @Override
+    public List<String> getAdditionalHelperClassNames() {
+        List<String> helpers = new ArrayList<>();
+        helpers.add(FrankSingletons.class.getName());
+        helpers.add(FrankPipeRequest.class.getName());
+        helpers.add(FrankPipeAttributeExtractor.class.getName());
+        helpers.add(FrankPipeSpanNameExtractor.class.getName());
+        helpers.add(FrankPipeStatusExtractor.class.getName());
+        return helpers;
     }
 }
